5b476ea3604a65a16bdb0aff412c9e38edf8815f
DuarteGe <111998883+DuarteGe@users.noreply.github.com>
2022-12-07 12:58:40 -0300

    Finito 2.6

--- CAP03/Código de Produto/script.js
+++ CAP03/Código de Produto/script.js
@@ -1,15 +1,36 @@
 function classificar(){
 var cod = document.getElementById("CodProd")
 var codi = parseInt(cod.value)
+var di = document.getElementById("res")
 switch(codi){
     case 1:
-        window.alert(`Alimento não-perecível`)
+        di.innerHTML = "Alimento não-perecível"
         break;
-    case 2||3||4:
-        window.alert(`Alimento perecível`)
+    case 2:
+        di.innerHTML = "Alimento perecível"
+        break;
+    case 3:
+        di.innerHTML = "Alimento perecível"
+        break;
+    case 4:
+        di.innerHTML = "Alimento perecível"
+        break;
+    case 5:
+        di.innerHTML = "Vestuário"
+        break;
+    case 6:
+        di.innerHTML = "Vestuário"
+        break;
+    case 7:
+        di.innerHTML = "Higiene Pessoal"
         break;
     default:
-        window.alert(`Outra coisa`);
+        if(codi>=8&&codi<=15){
+            di.innerHTML = "Limpeza e utensílios domésticos"
+
+        }else{
+            di.innerHTML = "Inválido"
+        }
 
 }
 
